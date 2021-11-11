a7a88eec7d1981286a941baa46abe6e16508d0fc
Kristen Bartel <kristenbartel@gmail.com>
2021-11-11 14:57:31 -0600

    added

---
+++ multipage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ function multipage() {
+    mkdir "$1";
+    cd "$1";
+    touch "index.html";
+    mkdir "scripts";
+    cd "scripts";
+    touch "scripts.js";
+    cd ..;
+    mkdir "styles";
+    cd "styles";
+    touch "styles.css";
+    cd ..;
+    code .
+ }
+
